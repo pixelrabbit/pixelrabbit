1bd3a4b7f3530c4ea64969a78b46600b53846987
pixelrabbit <dangovea@gmail.com>
2025-02-22 23:34:14 -0600

    setup

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,18 +1,14 @@
-"use client";
-
 import { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode;
   className?: string;
-  appName: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className }: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
     </button>
